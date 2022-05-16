e7a8bfd97349aa1e1d0b782125786ae3199e6045
Alyssa Rodriguez <alyssa.taylor.rodriguez@gmail.com>
2022-05-16 12:00:32 -0500

    Fixed from posts to blog.

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -1,19 +1,21 @@
-const { Comment } = require('../models');
+const { Comments } = require('../models');
 
 const commentData = [
     {
+        id: 1,
         user_id: 2,
-        post_id: 1,
+        blog_id: 1,
         comment_text: "This is amazing!"
     },
     {
+        id: 2,
         user_id: 1,
-        post_id: 2,
+        blog_id: 2,
         comment_text: "I am too!"
     },
 
 ]
 
-const seedComments = () => Comment.bulkCreate(commentData);
+const seedComments = () => Comments.bulkCreate(commentData);
 
 module.exports = seedComments;
--- seeds/index.js
+++ seeds/index.js
@@ -1,4 +1,4 @@
-const seedPosts = require('./post-seeds');
+const seedBlogs = require('./blog-seeds');
 const seedUsers = require('./user-seeds');
 const seedComments = require('./comment-seeds');
 
@@ -11,8 +11,8 @@ const seedAll = async () => {
   await seedUsers();
     console.log('\n----- USERS SEEDED -----\n');
   
-  await seedPosts();
-    console.log('\n----- POSTS SEEDED -----\n');
+  await seedBlogs();
+    console.log('\n----- BLOGS SEEDED -----\n');
 
   await seedComments();
     console.log('\n----- COMMENTS SEEDED -----\n');
